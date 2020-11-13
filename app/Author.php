b80a285d711932333fb6f74d336c5276979aac16
Mahdi Mahmoodian <mahmoodian.m1999@gmail.com>
2020-11-13 15:21:10 +0330

    updating book and author model to support new relation

--- app/Author.php
+++ app/Author.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
+use App\Models\Book;
 use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    public function books(){
+        return $this->belongsToMany(Book::class, 'books_authors');
+    }
 }
--- app/Book.php
+++ app/Book.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
+use App\Models\Author;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    public function authors(){
+        return $this->belongsToMany(Author::class, 'books_authors');
+    }
 }
