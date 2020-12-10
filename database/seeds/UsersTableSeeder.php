ada1ddfa3255f7e6415d12fb161e35eeaabbb6af
Mahdi Mahmoodian <mahmoodian.m1999@gmail.com>
2020-12-10 10:36:11 +0330

    adding new users with attaching roles

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,7 +13,11 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+        $admin_role = \App\Role::where('slug', 'admin')->first();
+        $staff_role = \App\Role::where('slug', 'staff')->first();
+        $patron_role = \App\Role::where('slug', 'patron')->first();
+
+        $admin = User::firstOrNew([
             'first_name' => "Mahdi",
             'last_name' => "Mahmoodian",
             'national_code' => "1234567890",
@@ -24,5 +28,49 @@ class UsersTableSeeder extends Seeder
             'email' => "mahmoodian.m1999@gmail.com",
             'password' => Hash::make("123456"),
         ]);
+        $admin->save();
+        $admin->roles()->save($admin_role);
+
+        $staff1 = User::firstOrNew([
+            'first_name' => "Danial",
+            'last_name' => "Moradi",
+            'national_code' => "1234567890",
+            'phone' => "22334455",
+            'mobile' => "09199069657",
+            'address' => "Tehran, Tehran",
+            'postal_code' => "12345",
+            'email' => "dan.ms@gmail.com",
+            'password' => Hash::make("123456"),
+        ]);
+        $staff1->save();
+        $staff1->roles()->save($staff_role);
+
+        $staff2 = User::firstOrNew([
+            'first_name' => "Keivan",
+            'last_name' => "Dehghan",
+            'national_code' => "1234567890",
+            'phone' => "22334455",
+            'mobile' => "09199069657",
+            'address' => "Tehran, Tehran",
+            'postal_code' => "12345",
+            'email' => "k1.dhg@gmail.com",
+            'password' => Hash::make("123456"),
+        ]);
+        $staff2->save();
+        $staff2->roles()->save($staff_role);
+
+        $patron = User::firstOrNew([
+            'first_name' => "Sarah",
+            'last_name' => "Firouz Abadi",
+            'national_code' => "1234567890",
+            'phone' => "22334455",
+            'mobile' => "09199069657",
+            'address' => "Tehran, Tehran",
+            'postal_code' => "12345",
+            'email' => "firouz.abadi@gmail.com",
+            'password' => Hash::make("123123"),
+        ]);
+        $patron->save();
+        $patron->roles()->save($patron_role);
     }
 }
